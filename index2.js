6b1d7eb742c13dbbf0bd64d2b7b43861fe89a582
real-empath <56172652+real-empath@users.noreply.github.com>
2020-07-10 01:22:25 -0500

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,13 @@
+const prefix = 'username\x00harry\x00';
+
+const post   = '\x00'
+
+const client = new net.Socket();
+
+client.connect(1024, '127.0.0.1', (err) => {
+
+   client.write(prefix + ‘some message’ + post);
+
+});
+
+client.on('data', (data) => response.toString());
